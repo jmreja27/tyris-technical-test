43c3c8a7940ede3221dd9184998c0a9b8de8d375
JuanjoMrv <jmuozrej@alumnos.unex.es>
2025-10-02 12:19:49 +0200

    Handle focus between input and grid

--- README.md
+++ README.md
@@ -6,9 +6,9 @@
 - npm >= 6.9
 
 ## Pasos a seguir
-1. Clonar el repositorio con `git clone xxxx`
+1. Clonar el repositorio con `git clone https://github.com/jmreja27/tyris-technical-test.git`
 2. Instalar las librerías necesarias con `npm install`
-3. Usar `ng serve -o` para desplegar en http://localhost:4200 o usar `ng build --prod` para generar el proyecto compilado en el directorio dist/tyris-technical-test
+3. Usar `ng serve -o` para desplegar en http://localhost:4200 o usar `ng build --prod` o `ng build` para generar el proyecto compilado en el directorio dist/tyris-technical-test
 
 ## Compatibilidad con navegadores antiguos
 - No se usa flexbox en CSS
@@ -17,3 +17,11 @@
 ### Traspilación a ES5
 - En el archivo .browserslistrc se ha habilitado como navegador compatible IE11
 - En el archivo tsconfig.json se ha puesto como target es5
+
+## Control de errores
+- Se ha añadido una página de error en el caso de que se acceda una ruta no contemplada. Se puede ver en el app.routing.module.ts
+- Se ha utilizado catchError en el archivo movie.service.st para controlar errores en caso de que la api de error
+- Para validar los datos, se comprueba que vengan los campos correctos en la respuesta. En todos los casos se muestra un texto por defecto a excepción de la imagen que se muestra el texto alternativo
+- En el componente movie-grid se controla que, en caso de que el navigate falle, el usuario siga en la pantalla inicial para evitar crasheos de la aplicación
+- En el componente movie-grid se muestra en pantalla un mensaje de error en caso de que la API falle
+- En el componente movie-detail se muestra en pantalla un mensaje de error en caso de que no exista la película con el nombre dado
--- src/app/components/movie-grid/movie-grid.component.ts
+++ src/app/components/movie-grid/movie-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, OnInit, QueryList, Renderer2, ViewChildren } from '@angular/core'
+import { Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core'
 import { Router } from '@angular/router'
 import { Movie } from 'src/app/models/movie.model'
 import { MoviesService } from 'src/app/services/movie.service'
@@ -12,26 +12,30 @@ export class MovieGridComponent implements OnInit {
   movies: Movie[] = []
   filteredMovies: Movie[] = []
   error: string = ''
-  columnas = 4
-  index = 0
-  circle = true
+  columnas: number = 4
+  index: number = 0
+  circle: boolean = true
   searchTerm: string = ''
+  focus: string = ''
 
   @ViewChildren('movie') refMovies!: QueryList<ElementRef>
+  @ViewChild('searchInput') refSearchInput!: ElementRef
   
   constructor(private moviesService: MoviesService, private renderer: Renderer2, private router: Router) { }
 
   ngOnInit(): void {
     this.moviesService.getMovies().subscribe(data => {
-      this.movies = data.movies
+      this.movies = data.movies 
       this.filteredMovies = data.movies
-      this.error = data.error
+      this.error = data.error || ''
     })
   }
 
   ngAfterViewInit(): void {
     this.refMovies.changes.subscribe(() => {
-      if(this.index === 0 && !this.searchTerm) this.focusByTabIndex(this.index)
+      if(this.index === 0 && !this.focus && this.filteredMovies.length > 0) {
+        this.setFocus('grid', this.index)
+      }
     })
   }
   
@@ -39,7 +43,6 @@ export class MovieGridComponent implements OnInit {
     if (movie) {
       this.router.navigate(['/movie', movie.title])
       .catch(() => {
-        this.error = 'No se se pudo acceder a los detalles de la Movie.'
         this.router.navigate(['/'])
       })
     }
@@ -50,11 +53,7 @@ export class MovieGridComponent implements OnInit {
     if(this.searchTerm !== '') {
       const filtered = this.movies.filter(item => item.title.toLowerCase().includes(term.toLowerCase()))
 
-      if (filtered.length === 0) {
-        this.error = 'No se han encontrado peliculas con ese titulo.'
-      } else {
-        this.error = ''
-      }
+      this.error = filtered.length === 0 ? 'No se han encontrado peliculas con ese titulo.' : ''
       this.filteredMovies = filtered
     } else {
       this.filteredMovies = this.movies
@@ -63,51 +62,87 @@ export class MovieGridComponent implements OnInit {
   }
 
   onHover(index: number) {
-    this.focusByTabIndex(index)
+    this.setFocus('grid', index)
   }
 
   onKeyDown(event: KeyboardEvent, currentIndex: number): void {
     let newIndex = currentIndex || this.index
     switch(event.key) {
       case 'Enter':
-        const movie = this.filteredMovies[newIndex]
-        if (movie) {
-          this.router.navigate(['/movie', movie.title])
+        if(this.focus === 'grid') {
+          const movie = this.filteredMovies[newIndex]
+          if (movie) {
+            this.router.navigate(['/movie', movie.title])
+            .catch(() => {
+              this.router.navigate(['/'])
+            })
+          }
         }
         break
       case 'ArrowRight':
-        if(newIndex + 1 <= this.movies.length - 1) newIndex += 1
-        else if(newIndex === this.movies.length - 1 && this.circle) newIndex = 0
+        if(this.focus === 'grid') {
+          if(newIndex + 1 <= this.filteredMovies.length - 1) newIndex += 1
+          else if(newIndex === this.filteredMovies.length - 1 && this.circle) newIndex = 0
+          this.setFocus('grid', newIndex)
+        }
         break
       case 'ArrowLeft':
-        if(newIndex - 1 >= 0) newIndex -= 1
-        else if(newIndex === 0 && this.circle) newIndex = this.movies.length - 1
+        if(this.focus === 'grid') {
+          if(newIndex - 1 >= 0) newIndex -= 1
+          else if(newIndex === 0 && this.circle) newIndex = this.filteredMovies.length - 1
+          this.setFocus('grid', newIndex)
+        }
         break
       case 'ArrowDown':
         const sumIndex = newIndex + this.columnas
-        if(sumIndex <= this.movies.length - 1) newIndex = sumIndex
-        else if(this.circle) newIndex = Number(sumIndex % this.columnas)
+        if(this.focus === 'search') {
+          this.setFocus('grid', this.index > this.filteredMovies.length - 1 ? 0 : this.index)
+        } else if(this.focus === 'grid') {
+          if(sumIndex <= this.filteredMovies.length - 1) newIndex = sumIndex
+          else if(this.circle) newIndex = Number(sumIndex % this.columnas)
+          this.setFocus('grid', newIndex)
+        }
         break
       case 'ArrowUp':
-        const subIndex = newIndex - this.columnas
-        if(subIndex >= 0) newIndex = subIndex
-        else if(this.circle) newIndex = this.movies.length + subIndex
+        if(this.focus === 'grid') {
+          const subIndex = newIndex - this.columnas
+          if(subIndex >= 0) {
+            newIndex = subIndex
+            this.setFocus('grid', newIndex)
+          } else this.setFocus('search')
+        }
         break
     }
-    event.preventDefault()
-    this.focusByTabIndex(newIndex)
+    if(this.focus === 'grid') event.preventDefault()
+  }
+
+  setFocus(element: string, index?: number) {
+    if(element === 'search') {
+      this.focus = 'search'
+      const focusElement = this.renderer.selectRootElement(this.refSearchInput.nativeElement, true)
+      const cardFocused = document.getElementsByClassName('focused')
+      if(cardFocused && cardFocused.length > 0) {
+        cardFocused[0].classList.remove('focused')
+      }
+      focusElement.focus()
+    } else if(element === 'grid') {
+      this.focus = 'grid'
+      this.focusByTabIndex(index || 0)
+    }
   }
 
   focusByTabIndex(tabIndex: number): void {
-    this.refMovies.forEach((ref, i) => {
-      if (i === tabIndex) {
-        this.index = tabIndex
-        this.renderer.addClass(ref.nativeElement, 'focused')
-        ref.nativeElement.focus()
-      } else {
-        this.renderer.removeClass(ref.nativeElement, 'focused')
-      }
-    })
+    if(this.refMovies && tabIndex >= 0 && this.filteredMovies.length > 0) {
+      this.refMovies.forEach((ref, i) => {
+        if (i === tabIndex) {
+          this.index = tabIndex
+          this.renderer.addClass(ref.nativeElement, 'focused')
+          ref.nativeElement.focus()
+        } else {
+          this.renderer.removeClass(ref.nativeElement, 'focused')
+        }
+      })
+    }
   }
 
 }
